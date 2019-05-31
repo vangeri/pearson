80ae69c43de6d58e119b6653662d21b49ccef322
vangeri <51209424+vangeri@users.noreply.github.com>
2019-05-30 20:39:04 -0600

    Part 1 style updated. Part 2 and 3 added

---
+++ part3.js
@@ -0,0 +1,40 @@
+    /* Transform Array Function */
+function transformNums(arr) {
+    arr.sort(function(a, b){return a - b}); /* Sort the array in ascending order */
+
+    var transformedArray = []; /* Initialize an empty array */
+    
+    /* loop 3 times while reversing the initial array and concatinating it to the new array*/
+    for (var i=0; i<3; i++) {
+        transformedArray = transformedArray.concat(arr);
+        arr.reverse();
+    }
+
+    /* Show the new array in console */
+    console.log(transformedArray);
+}
+
+/* List Generator */
+function listGenerator() {
+    const numItems = 100;
+    let listHtml = '<ul>';
+    for (var i = 1; i <= numItems; i++) {
+        
+        var textToAdd = (i%2 == 0) ? "EVEN" : "ODD";
+
+        textToAdd += (i%3 == 0) ? " 3 MULTIPLE" : "";
+
+        textToAdd += (i%5 == 0) ? " 5 MULTIPLE" : "";
+
+        listHtml += '<li>' + textToAdd + '</li>';
+    }
+
+    listHtml += '</ul>';
+
+    document.getElementById("showList").innerHTML = listHtml;
+}
+
+var arr = [3, 1, 2, 5, 4];
+transformNums(arr);
+
+listGenerator();
